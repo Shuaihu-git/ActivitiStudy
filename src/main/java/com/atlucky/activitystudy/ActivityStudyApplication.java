a9437e40c1fe457542a1aa66bf92689a51179ba8
shuaihu.zhang <shuaihu.zhang@9fzt.com.cn>
2023-11-20 15:41:48 +0800

    initial-commit:初始化基础配置

--- src/main/java/com/atlucky/activitystudy/ActivityStudyApplication.java
+++ src/main/java/com/atlucky/activitystudy/ActivityStudyApplication.java
@@ -1,9 +1,15 @@
 package com.atlucky.activitystudy;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 
-@SpringBootApplication
+/**
+ * @author shuaihu.zhang
+ */
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 public class ActivityStudyApplication {
 
     public static void main(String[] args) {
