b6c66a978a7138a525aff5f2fe9ad62c72cc221e
shuaihu.zhang <shuaihu.zhang@9fzt.com.cn>
2023-11-20 14:30:13 +0800

    initial-commit:初始化仓库

---
+++ src/main/java/com/atlucky/activitystudy/ActivityStudyApplication.java
@@ -0,0 +1,13 @@
+package com.atlucky.activitystudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atlucky/activitystudy/ActivityStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlucky.activitystudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
